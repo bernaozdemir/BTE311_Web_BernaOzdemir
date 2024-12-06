b7d3e122e1be55961ab394432fc9f1be9099334a
Berna ÖZDEMİR <99601260+bernaozdemir@users.noreply.github.com>
2024-12-06 15:24:07 +0300

    gbajbdkjabwjkdbakwbjd
    dawdjhabwdhbajhwbdjabwdjbajdbajhwbdjhabwdbajwbjdhaw

---
+++ yeniphp.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ders 11 falan</title>
+</head>
+<body>
+    
+
+ahwdbhbajwhdbjHABwdbajBdjhbaj
+awdbahjkbwdja
+
+
+
+
+</body>
+</html>
