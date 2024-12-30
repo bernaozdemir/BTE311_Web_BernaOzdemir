487f65cd270bd4c55b1a77de8bb506b3330216fc
Berna ÖZDEMİR <99601260+bernaozdemir@users.noreply.github.com>
2024-12-30 20:56:43 +0300

    Update hafta12.php

---
+++ hafta12.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hafta 12 Görevleri</title>
+</head>
+<body>
+    <h1>Hafta 12 Görevleri</h1>
+    <nav>
+        <ul>
+            <li><a href="hafta12_tek_sayilar.php" target="_blank">1-100 Arası Tek Sayılar</a></li>
+            <li><a href="hafta12_tablo_olustur.php" target="_blank">Dinamik Tablo Oluştur</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
