9d9bcd90187b4921718e3c94d8fbd2d733654361
Berna ÖZDEMİR <99601260+bernaozdemir@users.noreply.github.com>
2024-12-31 00:37:26 +0300

    Update hafta13.php

--- hafta13.php
+++ hafta13.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kişi Yönetim Sistemi</title>
+    <title>Hafta 13 - Kişi Yönetim Sistemi</title>
     <style>
         body {
             font-family: Arial, sans-serif;
